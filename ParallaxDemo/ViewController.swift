ae5abce3f6d197e206d0a446cee52206d1d928bb
Michael Edenzon <edenzonmp@g.cofc.edu>
2018-03-13 22:14:44 -0400

    Initial Commit

---
+++ ParallaxDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParallaxDemo
+//
+//  Created by Michael Edenzon on 3/13/18.
+//  Copyright © 2018 Michael Edenzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
